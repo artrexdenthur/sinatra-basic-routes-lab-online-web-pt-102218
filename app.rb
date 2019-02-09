a29591861df7b0547d8b2b5f7e2c6ed3f3c9869a
Paul Ashour <paul.ashour@gmail.com>
2019-02-09 04:31:19 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,12 +2,14 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get "/name" do
-    
+    "My name is Lucy"
   end
   
   get "/hometown" do
+    "My hometown is Arlington, TX"
   end
   
   get "/favorite-song" do
+    "My favorite song is 'Something'"
   end
 end
